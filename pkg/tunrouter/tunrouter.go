a1b520502b652242f514d55101f0a349dcffa7c3
Ryan Prairie <me@ryanprairie.com>
2022-02-13 12:40:38 -0500

    Add: a more complete tunrouter.Run

--- pkg/tunrouter/tunrouter.go
+++ pkg/tunrouter/tunrouter.go
@@ -1,15 +1,18 @@
 package tunrouter
 
 import (
-	"github.com/prairir/Buoy/pkg/config"
 	"github.com/prairir/Buoy/pkg/ethrouter"
 
 	"github.com/songgao/water"
 	"golang.org/x/sync/errgroup"
 )
 
-func Run(eg *errgroup.Group, inf *water.Interface,  chan ethrouter.Packet, readq chan []byte) error {
-	eg.Go(func() error{
-		return writer(, writeq chan []byte)
+func Run(eg *errgroup.Group, inf *water.Interface, readq chan ethrouter.Packet, writeq chan []byte) error {
+	eg.Go(func() error {
+		return writer(inf, writeq)
+	})
+
+	eg.Go(func() error {
+		return reader(inf, readq)
 	})
 }
